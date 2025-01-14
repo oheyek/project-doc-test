74de525bf2230e8589794171f14fe83b91df5002
oheyek <oheyek@proton.me>
2025-01-14 21:17:40 +0100

    3rd file

---
+++ src/test.py
@@ -0,0 +1,18 @@
+"""
+Example Module 3 - docstring test
+--------------
+This module demonstrates how to use docstrings for documentation.
+"""
+
+def multiply_numbers(a, b):
+    """
+    Multiply two numbers and returns the result.
+
+    Parameters:
+    a (int): The first number.
+    b (int): The second number.
+
+    Returns:
+    int / float: The subtraction of a and b.
+    """
+    return a * b
