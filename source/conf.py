8b3c5ff306e75dbe4c7a251939c4d50f5425e6f1
oheyek <oheyek@proton.me>
2025-01-28 21:45:39 +0100

    lang change

--- source/conf.py
+++ source/conf.py
@@ -19,7 +19,7 @@ extensions = ['sphinx.ext.autodoc', 'breathe']
 templates_path = ['_templates']
 exclude_patterns = []
 
-language = 'Polski'
+language = 'English'
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
