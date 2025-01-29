e27659b91da8a6090dc997489d612b9bc798fac3
oheyek <oheyek@users.noreply.github.com>
2025-01-29 10:14:40 +0000

    deploy: 61a8dd598c3cb03565f34e5c77b1bce488a2a8c9

--- doxygen/example2_8py.js
+++ doxygen/example2_8py.js
@@ -1,4 +1,4 @@
 var example2_8py =
 [
-    [ "example2.subtract_numbers", "namespaceexample2.html#ada60000ca97abf30119c09e7b1b6e362", null ]
+    [ "subtract_numbers", "example2_8py.html#ada60000ca97abf30119c09e7b1b6e362", null ]
 ];
